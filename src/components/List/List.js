a98ae84d0ab80ff0f04c227d41eca2e44dfe6a34
Gico735 <gic0@bk.ru>
2018-11-04 22:25:02 +0300

    make normas  animate

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -12,23 +12,33 @@ class List extends React.Component {
         {
           id: 1,
           desc: 'test_1',
-          done: false
+          done: false,
+          posId: 0
         },
         {
           id: 2,
           desc: 'test_2',
-          done: false
+          done: false,
+          posId: 1
         },
         {
           id: 3,
           desc: 'test_3',
-          done: false
+          done: false,
+          posId: 2
         },
         {
           id: 4,
           desc: 'test_4',
-          done: false
+          done: false,
+          posId: 3
         },
+        {
+          id: 5,
+          desc: 'test_5',
+          done: false,
+          posId: 4
+        }
       ]
     }
   }
@@ -80,19 +90,46 @@ class List extends React.Component {
     this.sortTasks(tasks)
   }
 
+  // sortTasks = (tasks) => {
+  //   tasks.forEach((el, i) => {
+  //     tasks.forEach((elem, j) => {
+  //       if (j === tasks.length - 1) return
+  //       if (i === tasks.length - 1) return
+  //       console.log(el.id, el.posId, '>', tasks[j + 1].posId, tasks[j + 1].id, 'asdasd', i)
+  //       if ((el.done < tasks[j + 1].done) && (el.posId < tasks[j + 1].posId)) {
+  //         const old = el.posId
+  //         tasks[i].posId = tasks[j + 1].posId
+  //         tasks[j + 1].posId = old
+  //         console.log('asd')
+  //       } else if (el.done === tasks[j + 1].done) {
+  //         if (el.posId > tasks[j + 1].posId) {
+  //           const old = el.posId
+  //           tasks[i].posId = tasks[j + 1].posId
+  //           tasks[j + 1].posId = old
+  //           console.log('qwe')
+  //         }
+  //       }
+  //     })
+  //   })
+  //   this.setState({ tasks })
+  // }
+
   sortTasks = (tasks) => {
-    tasks.map(() => {
-      tasks.map((elem, i) => {
-        if (i === tasks.length - 1) return
-        if (elem.done < tasks[i + 1].done) {
-          const old = elem
-          tasks[i] = tasks[i + 1]
-          tasks[i + 1] = old
-        } else if (elem.done === tasks[i + 1].done) {
-          if (elem.id > tasks[i + 1].id) {
-            const old = elem
-            tasks[i] = tasks[i + 1]
-            tasks[i + 1] = old
+    tasks.forEach(el1 => {
+      tasks.forEach(el2 => {
+        if (el1.id === el2.id || el1.id > el2.id) return
+        if ((el1.done < el2.done) && (el1.posId < el2.posId)) {
+          const old = el1.posId
+          el1.posId = el2.posId
+          el2.posId = old
+        } else if ((el1.done === el2.done)) {
+          if (el1.id === 3 || el1.id === 1) {
+            console.log(el1.id, el2.id)
+          }
+          if ((el1.posId > el2.posId) && (el1.id < el2.id)) {
+            const old = el1.posId
+            el1.posId = el2.posId
+            el2.posId = old
           }
         }
       })
@@ -104,21 +141,21 @@ class List extends React.Component {
     const { input, tasks } = this.state
     return (
       <div className='List'>
-        <div className='add'>
+        <div className='list__add'>
           <input value={input} onChange={this.changeInput} type='text' className='add__input' />
           <button onClick={this.addTask} className='add__btn'>ADD</button>
         </div>
         <div className='list__wrap'>
-          <FlipMove>
-            {
-              tasks.map(el => {
-                console.log(el)
-                return (
-                  <ListItem key={el.id} task={el} checkTask={this.checkTask} removeTask={this.removeTask} />
-                )
-              })
-            }
-          </FlipMove>
+          {/* <FlipMove> */}
+          {
+            tasks.map(el => {
+              console.log(el)
+              return (
+                <ListItem key={el.id} task={el} checkTask={this.checkTask} removeTask={this.removeTask} />
+              )
+            })
+          }
+          {/* </FlipMove> */}
         </div>
       </div>
     )
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -3,27 +3,17 @@ import './ListItem.css'
 
 
 class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...this.props.task,
-    }
-  }
-
-  componentWillReceiveProps() {
-    this.setState({
-      ...this.props.task
-    })
-  }
 
   render() {
-    const { id , desc, done} = this.state
+    const { id, desc, done, posId } = this.props.task
+    const { checkTask, removeTask } = this.props
+    const top = 135 * posId;
     return (
-      <div className='listitem'>
+      <div className='listitem' style={{top: top}}>
         <div className='listitem__key'>{id}</div>
         <div className='listitem__name'>{desc}</div>
-        <div data-task={id} onClick={this.props.checkTask}  className='listitem__check'>{done === false ? 'false' : 'true'}</div>
-        <div data-task={id} onClick={this.props.removeTask} className='listitem__remove'>[X]</div>
+        <div data-task={id} onClick={checkTask} className='listitem__check'>{done === false ? 'false' : 'true'}</div>
+        <div data-task={id} onClick={removeTask} className='listitem__remove'>[X]</div>
       </div>
     )
   }
